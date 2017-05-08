619085fc77b1bfec7267fba4af333de6f1b8f1c6
robert-hatfield <robert-hatfield@users.noreply.github.com>
2017-05-08 11:23:42 -0700

    Add Firebase to AppDelegate, create login VC

---
+++ Todo Anywhere/Todo Anywhere/LoginViewController.m
@@ -0,0 +1,27 @@
+//
+//  LoginViewController.m
+//  Todo Anywhere
+//
+//  Created by Robert Hatfield on 5/8/17.
+//  Copyright © 2017 Robert Hatfield. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *emailTextField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+
+@end
