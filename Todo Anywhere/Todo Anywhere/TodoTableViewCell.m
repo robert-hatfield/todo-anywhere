399dff7ff7b84742a21bb1ac4db091379f693d8f
robert-hatfield <robert-hatfield@users.noreply.github.com>
2017-05-08 18:29:59 -0700

    Add Todo model class and custom tableview cell

---
+++ Todo Anywhere/Todo Anywhere/TodoTableViewCell.m
@@ -0,0 +1,28 @@
+//
+//  TodoTableViewCell.m
+//  Todo Anywhere
+//
+//  Created by Robert Hatfield on 5/8/17.
+//  Copyright © 2017 Robert Hatfield. All rights reserved.
+//
+
+#import "TodoTableViewCell.h"
+
+@interface TodoTableViewCell ()
+
+@end
+
+@implementation TodoTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
