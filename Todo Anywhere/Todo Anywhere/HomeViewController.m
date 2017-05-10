d3c651a81cc1610d120f7924d6bde76a7ecfdfea
robert-hatfield <robert-hatfield@users.noreply.github.com>
2017-05-10 10:15:51 -0700

    Remove unused code from Home VC

--- Todo Anywhere/Todo Anywhere/HomeViewController.m
+++ Todo Anywhere/Todo Anywhere/HomeViewController.m
@@ -12,25 +12,15 @@
 #import "TodoTableViewCell.h"
 #import "TodoDatabase.h"
 
-//@import FirebaseAuth;
-//@import Firebase;
-
 static CGFloat const kClosedConstraint = 0.0;
 static CGFloat const kOpenConstraint = 150.0;
 static NSTimeInterval const kShortAnimationDuration = 0.34;
 
 @interface HomeViewController () <UITableViewDataSource>
 
-//@property(strong, nonatomic) FIRDatabaseReference *userReference;
-//@property(strong, nonatomic) FIRUser *currentUser;
-//
-//@property(nonatomic) FIRDatabaseHandle allTodosHandler;
-//
-//@property (strong, nonatomic) NSMutableArray *allTodos;
 @property (weak, nonatomic) IBOutlet UIBarButtonItem *logOutButton;
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *createTodoHeightConstraint;
 @property (weak, nonatomic) IBOutlet UITableView *todoTableView;
-
 
 @end
 
@@ -59,51 +49,6 @@ static NSTimeInterval const kShortAnimationDuration = 0.34;
 - (void)dealloc {
     [[NSNotificationCenter defaultCenter]removeObserver:self name:@"todosChanged" object:nil];
 }
-
-//- (void)checkUserStatus {
-//    // Present login view if user is not logged in, otherwise setup Firebase references.
-//    if (![[FIRAuth auth] currentUser]) {
-//        LoginViewController *loginController = [self.storyboard instantiateViewControllerWithIdentifier:@"LoginViewController"];
-//        [self presentViewController:loginController animated:YES completion:nil];
-//    } else {
-//        [self setupFirebase];
-//        [self startMonitoringTodoUpdates];
-//        [self.logOutButton setEnabled:YES];
-//    }
-//}
-
-//- (void)setupFirebase {
-//    FIRDatabaseReference *databaseReference = [[FIRDatabase database] reference];
-//    self.currentUser = [[FIRAuth auth] currentUser];
-//    self.userReference = [[databaseReference child:@"users"] child:self.currentUser.uid];
-//    NSLog(@"User reference: %@", self.userReference);
-//}
-
-//- (void)startMonitoringTodoUpdates {
-//    self.allTodosHandler = [[self.userReference child:@"todos"] observeEventType:FIRDataEventTypeValue withBlock:^(FIRDataSnapshot * _Nonnull snapshot) {
-//        
-//        self.allTodos = [[NSMutableArray alloc] init];
-//        
-//        for (FIRDataSnapshot *child in snapshot.children) {
-//            NSDictionary *todoData = child.value;
-//            NSString *todoTitle = todoData[@"title"];
-//            NSString *todoContent = todoData[@"content"];
-//            NSNumber *boolNumber = todoData[@"isCompleted"];
-//            Boolean isCompleted = boolNumber.boolValue;
-//            
-//            // TODO: Append new Todo to allTodos array
-//            NSLog(@"Todo Title: %@ - Content: %@", todoTitle, todoContent);
-//            Todo *currentTodo = [[Todo alloc] init];
-//            currentTodo.title = todoTitle;
-//            currentTodo.content = todoContent;
-//            currentTodo.isCompleted = isCompleted;
-//            [self.allTodos addObject:currentTodo];
-//        }
-//        [self.todoTableView reloadData];
-//    }];
-//}
-
-
 
 //MARK: Tableview datasource methods
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
@@ -146,7 +91,6 @@ static NSTimeInterval const kShortAnimationDuration = 0.34;
     [UIView animateWithDuration:kShortAnimationDuration animations:^{
         [self.view layoutIfNeeded];
     }];
-    
 }
 
 @end
